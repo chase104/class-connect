00b6ea3b55a76f208d877112b56a313f61baaf9d
dzou <dzou@company.com>
2021-02-26 09:42:12 +0100

    change port variable for Heroku deploy

--- server.js
+++ server.js
@@ -162,7 +162,7 @@ app.get('/api/customers', (req, res) => {
 });
 
 
-const port = process.env.PORT || 5000;
+const port = process.env.SERVERPORT || 5000;
 console.log(port);
 app.listen(port, () =>
   console.log(
